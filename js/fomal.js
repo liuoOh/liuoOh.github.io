455edcd4268096f0ba642f44852759208ffb9081
liuoOh <1727495114@qq.com>
2026-07-10 16:00:38 +0800

    Site updated: 2026-07-10 16:00:38

--- js/fomal.js
+++ js/fomal.js
@@ -3174,10 +3174,10 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/dm14.webp);
-    --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
-    --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
-    --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
+    --default-bg: url(/assets/bg_clean.jpg);
+    --darkmode-bg:url(/assets/bg_clean.jpg);
+    --mobileday-bg: url(/assets/bg_clean.jpg);
+    --mobilenight-bg: url(/assets/bg_clean.jpg);
   }`;
 }
 // 切换背景主函数
